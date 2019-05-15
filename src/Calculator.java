1b15eef2df3191943cdd2196411c52a50f445436
yvnrCleartrip <yekkaluri.reddy@gmail.com>
2019-05-15 18:59:00 +0530

    addition function added

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(calculator.addition(x,y));
+    }
+    public int addition(int x,int y){
+        return x+y;
+    }
+}
